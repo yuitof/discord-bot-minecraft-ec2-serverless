402f9f462811494cd62db61792c1f704c180682f
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-29 08:33:17 -0700

    Add function to start an EC2 instance via boto3

--- src/app.py
+++ src/app.py
@@ -11,11 +11,10 @@ import time
 PUBLIC_KEY = os.environ.get('APPLICATION_PUBLIC_KEY')
 S3_BUCKET = os.environ.get('S3_BUCKET')
 OBJECT_KEY = os.environ.get('OBJECT_KEY')
+INSTANCE_ID = os.environ.get('INSTANCE_ID')
+# REGION = os.environ.get("REGION")
 
 app = Flask(__name__)
-
-# ec2 = boto3.resource('ec2', region_name=os.environ.get("EC2_INSTANCE_REGION"))
-# instance = ec2.Instance(os.environ.get("EC2_INSTANCE_ID"))
 
 @app.route('/interactions', methods=['POST'])
 @verify_key_decorator(PUBLIC_KEY)
@@ -31,20 +30,22 @@ def interactions():
                 }
             })
         if data['data']['name'] == 'test':
-            start_time = time.time()
             s3_client = boto3.client('s3')
             try:
-                print('storing data in s3')
                 response = s3_client.put_object(Bucket=S3_BUCKET, Key=OBJECT_KEY, Body=json.dumps(data))
                 print('response from s3', response)
             except ClientError as e:
                 print('error:', e)
-            # instance.start_instance()
-            end_time = time.time()
-
-            elapsed_time = end_time - start_time
-            print(f"Process took {elapsed_time:.4f} seconds")
             
+            ec2_client = boto3.client('ec2')
+            try:
+                response = ec2_client.start_instances(
+                    InstanceIds=[ INSTANCE_ID ]
+                )
+                print('response from ec2:', response)
+            except ClientError as e:
+                print('error:', e)
+
             return jsonify({
                 'type': InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
             })
